2b757c92a1bf6c395c06732890b1cede70fd2bda
badie <gustavbadie@gmail.com>
2021-10-20 09:58:00 +0330

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./assets/css/styles.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
